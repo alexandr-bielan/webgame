9a68576cab890e76c3c53300d472136122ec9e69
Alex <kp9k999@gmail.com>
2021-02-28 13:31:24 +0200

    updt1

--- src/test/java/com/web/server/webserver/WebserverApplicationTests.java
+++ src/test/java/com/web/server/webserver/WebserverApplicationTests.java
@@ -1,13 +1,15 @@
+package com.web.server.webserver;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 
 @SpringBootTest
+
 public class WebserverApplicationTests {
     @Test
     void contextLoads() {
